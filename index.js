66c384407790bf6dca25752a97c0fedd9cf46be6
MarkoVoitovych <searchangeos@gmail.com>
2023-01-29 18:35:31 +0200

    clifailed

--- index.js
+++ index.js
@@ -22,18 +22,7 @@ program
   .option('-e, --email <type>', 'user email')
   .option('-p, --phone <type>', 'user phone');
 
-program.parse();
-
-const argv = program.opts();
 const allActions = ['list', 'get', 'add', 'edit', 'remove', 'clear'];
-
-const isValidInput = value => {
-  if (value < 1 || value > 10) {
-    console.log('Число має бути в діапазоні від 1 до 10'.red);
-    return false;
-  }
-  return true;
-};
 
 const invokeAction = async ({ action, id, name, email, phone }) => {
   switch (action) {
@@ -66,37 +55,10 @@ const invokeAction = async ({ action, id, name, email, phone }) => {
       break;
 
     case 'clear':
-      console.log(removedContact);
+      rl.close();
       break;
 
     default:
       console.warn('\x1B[31m Unknown action type!');
   }
 };
-
-// invokeAction(argv);
-
-const game = () => {
-  rl.question(
-    'Введіть число від 1 до 10, щоб вгадати задумане: '.yellow,
-    value => {
-      console.log(value);
-      console.log(typeof value);
-      // let a = +value;
-      // if (!isValid(a)) {
-      //   game();
-      //   return;
-      // }
-      // count += 1;
-      // if (a === mind) {
-      //   console.log('Вітаю Ви вгадали число за %d крок(ів)'.green, count);
-      //   rl.close();
-      //   return;
-      // }
-      // console.log('Ви не вгадали ще спроба'.red);
-      // game();
-    },
-  );
-};
-
-game();
