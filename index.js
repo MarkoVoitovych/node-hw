31062111f0bc9a9afb65b7714e6fb64da293eb98
MarkoVoitovych <searchangeos@gmail.com>
2023-01-29 16:06:03 +0200

    CLI started

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const { program } = require('commander');
+const readline = require('readline');
+require('colors');
 
 const { listContacts } = require('./listContacts');
 const {
@@ -8,6 +10,11 @@ const {
   addContact,
 } = require('./contacts');
 
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
 program
   .option('-a, --action <type>', 'choose action')
   .option('-i, --id <type>', 'user id')
@@ -15,9 +22,18 @@ program
   .option('-e, --email <type>', 'user email')
   .option('-p, --phone <type>', 'user phone');
 
-program.parse(process.argv);
+// program.parse(process.argv);
 
 const argv = program.opts();
+const allActions = ['list', 'get', 'add', 'edit', 'remove', 'clear'];
+
+const isValidInput = value => {
+  if (value < 1 || value > 10) {
+    console.log('Число має бути в діапазоні від 1 до 10'.red);
+    return false;
+  }
+  return true;
+};
 
 const invokeAction = async ({ action, id, name, email, phone }) => {
   switch (action) {
@@ -51,9 +67,38 @@ const invokeAction = async ({ action, id, name, email, phone }) => {
       console.log(removedContact);
       break;
 
+    case 'clear':
+      console.log(removedContact);
+      break;
+
     default:
       console.warn('\x1B[31m Unknown action type!');
   }
 };
 
-invokeAction(argv);
+// invokeAction(argv);
+
+const game = () => {
+  rl.question(
+    'Введіть число від 1 до 10, щоб вгадати задумане: '.yellow,
+    value => {
+      console.log(value);
+      console.log(typeof value);
+      // let a = +value;
+      // if (!isValid(a)) {
+      //   game();
+      //   return;
+      // }
+      // count += 1;
+      // if (a === mind) {
+      //   console.log('Вітаю Ви вгадали число за %d крок(ів)'.green, count);
+      //   rl.close();
+      //   return;
+      // }
+      // console.log('Ви не вгадали ще спроба'.red);
+      // game();
+    },
+  );
+};
+
+game();
